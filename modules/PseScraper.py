7a66ef1aefcb59acbe25689c4eb9aba44e503c2b
ketchupyuu <ketchupyuu@gmail.com>
2017-08-07 16:36:35 +0800

    Create scraper module

---
+++ modules/PseScraper.py
@@ -0,0 +1,26 @@
+import urllib2
+
+
+class PseScraper:
+    def init(self):
+        pass
+
+    def get_html_response(self,url):
+        try:
+            req = urllib2.Request(url)
+            req.add_header('Cache-Control','max-age=0')
+            content = urllib2.urlopen(req)
+        except urllib2.HTTPError as e:
+            print('HTTP Error:', e.code,url)
+        except urllib2.URLError as e:
+            print('URL Error:',e.code,url)
+
+    def download_from_url(self,dl_url,file_path):
+        content = self.get_html_response(dl_url)
+        try:
+            with open(file_path,"wb") as targ_file:
+                targ_file.write(content)
+            print("Content downloaded to %s" % file_path)
+        except IOError as e:
+            print('Could not write to file [%s] % file_path')
+            print('ERROR:',e.errorno,e.strerror) 
